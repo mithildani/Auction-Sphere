7dca1482c3b93d3e252d40d203cc7b5473219037
Shreya Maheshwari <shreya.2506@gmail.com>
2022-10-03 17:26:55 -0400

    Added Flask_CORS to setup.py

--- backend/setup.py
+++ backend/setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(where='backend'),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask']
+    install_requires=['Flask','flask-cors']
 )
