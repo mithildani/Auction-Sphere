5ff2553e5de7c562e255bc582b3ff1af8ee61970
Kartik Soni <ksoni@ncsu.edu>
2022-10-03 17:27:42 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+// import { Routes ,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './components/LoginSignup/Signup';
+import Login from './components/LoginSignup/Login';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Signup />}>
+          {/* <Route index element={<Signup />} /> */}
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
