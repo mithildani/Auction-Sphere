46c28f2e4dfe819e4fde073c12ab063f5519d8a8
Kartik Soni <kartiksoni2209@gmail.com>
2022-10-04 13:08:53 -0400

    Signup form done

--- src/components/LoginSignup/Signup.js
+++ src/components/LoginSignup/Signup.js
@@ -1,31 +1,109 @@
-import React from "react";
-import { Form, FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import About from "../About";
 import Navv from "../Navv";
 const Signup = () => {
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.password !== formData.confirmPassword)
+      alert("Passwords do not match");
+    console.log(formData);
+  };
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    contact: "",
+    email: "",
+    address: "",
+    password: "",
+    confirmPassword: "",
+  });
   return (
     <div>
       <Navv />
       Signup
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
+          <Label for="FirstName">First Name</Label>
           <Input
-            id="exampleEmail"
+            id="FirstName"
+            name="fname"
+            placeholder="First Name"
+            type="text"
+            value={formData.fname}
+            onChange={(e) => handleChange(e)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="LastName">Last Name</Label>
+          <Input
+            id="LastName"
+            name="lname"
+            placeholder="Last Name"
+            type="text"
+            value={formData.lname}
+            onChange={(e) => handleChange(e)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Contact">Contact</Label>
+          <Input
+            id="Contact"
+            name="contact"
+            placeholder="Contact"
+            type="text"
+            value={formData.contact}
+            onChange={(e) => handleChange(e)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Email">Email</Label>
+          <Input
+            id="Email"
             name="email"
-            placeholder="with a placeholder"
+            placeholder="Enter your email here"
             type="email"
+            value={formData.email}
+            onChange={(e) => handleChange(e)}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
+          <Label for="Address">Address</Label>
           <Input
-            id="examplePassword"
-            name="password"
-            placeholder="password placeholder"
-            type="password"
+            id="Address"
+            name="address"
+            placeholder="Address"
+            type="textarea"
+            value={formData.address}
+            onChange={(e) => handleChange(e)}
           />
         </FormGroup>
+        <FormGroup>
+          <Label for="Password">Password</Label>
+          <Input
+            id="Password"
+            name="password"
+            placeholder="password"
+            type="password"
+            value={formData.password}
+            onChange={(e) => handleChange(e)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="ConfirmPassword">Confirm Password</Label>
+          <Input
+            id="ConfirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+            type="password"
+            value={formData.confirmPassword}
+            onChange={(e) => handleChange(e)}
+          />
+        </FormGroup>
+        <Button>Submit</Button>
       </Form>
       <br />
       <div style={{ marginLeft: "1rem" }}>
