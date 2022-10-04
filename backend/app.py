c121e048f1ebef196af295c15f7bd7c8737775a8
Nandini Mundra <nandinimundra00@gmail.com>
2022-10-04 13:00:32 -0400

    create product API

--- backend/app.py
+++ backend/app.py
@@ -104,45 +104,36 @@ def create_bid():
         response["message"]="Saved Bid"
     return jsonify(response)
 
-@app.route("/create-predict", methods=["POST"])
+@app.route("/product/create", methods=["POST"])
 def create_product():
-    #Get all the product details
-    productId = request.get_json()['prod_id']
     productName = request.get_json()['name']
     sellerEmail = request.get_json()['seller_email']
-    intialPrice = request.get_json()['initial_price']
-    date = request.get_json()['date']
+    initialPrice = request.get_json()['initial_price']
+    # date = request.get_json()['date']
     increment = request.get_json()['increment']
     deadlineDate = request.get_json()['deadline_date']
     description = request.get_json()['description']
     
     conn = create_connection(database)
     c = conn.cursor()
-    
-    #check if product is already there
-    query = "SELECT COUNT(*) FROM product WHERE productId ='" + str(productId) + "';"
-    c.execute(query)
-    
-    result = list(c.fetchall())
     response = {}
-    if(result[0][0] == 0): 
-        query = "INSERT INTO product(prod_id, name, seller_email, initial_price, date, increment, deadline_date, description) VALUES('" + str(productId) + "','" + str(productName) + "','" + str(sellerEmail) + "','" + str(initialPrice) + "','" + str(date) + "','" + str(increment) + "','" + str(deadlineDate) + "','" + str(description) + "');"
-        c.execute(query)
-        conn.commit()
-        response["result"] = "Added product successfully"
-    else:
-        response["result"] = "This product already exists"
+    query = "INSERT INTO product( name, seller_email, initial_price, date, increment, deadline_date, description) VALUES('" + str(productName) + "','" + str(sellerEmail) + "'," + str(initialPrice) + ",'datetime()'," + str(increment) + ",'" + str(deadlineDate) + "','" + str(description) + "');"
+    print(query)
+    c.execute(query)
+    conn.commit()
+    response["result"] = "Added product successfully"
+
     return response
 
 database = r"auction.db"
-#write queries for creating database here:
-create_users_table = """CREATE TABLE users( first_name TEXT NOT NULL, last_name TEXT NOT NULL, contact_number TEXT NOT NULL UNIQUE, email TEXT UNIQUE PRIMARY KEY, password TEXT NOT NULL);"""
+# write queries for creating database here:
+create_users_table = """CREATE TABLE IF NOT EXISTS users( first_name TEXT NOT NULL, last_name TEXT NOT NULL, contact_number TEXT NOT NULL UNIQUE, email TEXT UNIQUE PRIMARY KEY, password TEXT NOT NULL);"""
 
-create_product_table = """CREATE TABLE product(prod_id INTEGER PRIMARY KEY, name TEXT NOT NULL, seller_email TEXT NOT NULL, initial_price REAL NOT NULL, date INTEGER NOT NULL, increment REAL, deadline_date INTEGER NOT NULL, description TEXT,  FOREIGN KEY(seller_email) references users(email));"""
+create_product_table = """CREATE TABLE IF NOT EXISTS product(prod_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, seller_email TEXT NOT NULL, initial_price REAL NOT NULL, date INTEGER NOT NULL, increment REAL, deadline_date INTEGER NOT NULL, description TEXT,  FOREIGN KEY(seller_email) references users(email));"""
 
-create_bids_table = """CREATE TABLE bids(prod_id INTEGER, email TEXT NOT NULL , bid_amount REAL NOT NULL, FOREIGN KEY(email) references users(email), FOREIGN KEY(prod_id) references product(prod_id) PRIMARY KEY(prod_id, email);"""
+create_bids_table = """CREATE TABLE IF NOT EXISTS bids(prod_id INTEGER, email TEXT NOT NULL , bid_amount REAL NOT NULL, FOREIGN KEY(email) references users(email), FOREIGN KEY(prod_id) references product(prod_id), PRIMARY KEY(prod_id, email));"""
 
-create_table_claims = """CREATE TABLE claims(prod_id INTEGER, email TEXT NOT NULL, expiry_date TEXT NOT NUL, claim_status INTEGER, FOREIGN KEY(email) references users(email), FOREIGN KEY(prod_id) references product(prod_id));"""
+create_table_claims = """CREATE TABLE IF NOT EXISTS claims(prod_id INTEGER, email TEXT NOT NULL, expiry_date TEXT NOT NULL, claim_status INTEGER, FOREIGN KEY(email) references users(email), FOREIGN KEY(prod_id) references product(prod_id));"""
 
 conn = create_connection(database)
 if conn is not None:
@@ -153,3 +144,6 @@ if conn is not None:
 else:
     print("Error! Cannot create the database connection")
 
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
