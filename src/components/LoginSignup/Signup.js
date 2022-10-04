a6de94816538ebce98cb1547136f9b9c18c1f56e
Kartik Soni <ksoni@ncsu.edu>
2022-10-03 21:05:07 -0400

    Navbar throughout the application

--- src/components/LoginSignup/Signup.js
+++ src/components/LoginSignup/Signup.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import { Form, FormGroup, Label, Input} from 'reactstrap'
+import React from "react";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+import Navv from "../Navv";
 const Signup = () => {
   return (
-    <div>Signup
+    <div>
+      <Navv />
+      Signup
       <Form>
-      <FormGroup>
-        <Label for="exampleEmail">
-          Email
-        </Label>
-        <Input
-          id="exampleEmail"
-          name="email"
-          placeholder="with a placeholder"
-          type="email"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">
-          Password
-        </Label>
-        <Input
-          id="examplePassword"
-          name="password"
-          placeholder="password placeholder"
-          type="password"
-        />
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="with a placeholder"
+            type="email"
+          />
         </FormGroup>
-    </Form>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="password placeholder"
+            type="password"
+          />
+        </FormGroup>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
