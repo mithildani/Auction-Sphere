e5ec587b1b168658577534dcd6fbd24070214d4d
palash27 <rathodpalash3@gmail.com>
2022-10-05 19:12:47 -0400

    Test case for Navbar tabs

--- src/tests/Navv.test.js
+++ src/tests/Navv.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Navv from "../components/Navv";
 
+const tabs = [
+  { text: "Signup", location: "/" },
+  { text: "Login", location: "/login" },
+  { text: "Products", location: "/products" },
+];
 test("renders Nav bar", () => {
   render(<Navv />);
   const linkElement = screen.getByText(/Products/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test.each(tabs)("Each tab working", (tab) => {
+  render(<Navv />);
+  const tab_link = screen.getByText(tab.text);
+  expect(tab_link).toHaveAttribute("href", tab.location);
 });
