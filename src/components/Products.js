abad77fbdb763d58b41ce5dd0800a219bbac48ec
palash27 <rathodpalash3@gmail.com>
2022-10-05 13:52:43 -0400

    Integration for Get products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Navv from "./Navv";
 import ProductCard from "./ProductCard";
+import { URL } from "../global";
+import axios from "axios";
 
 const Products = () => {
+  const [allProducts, setAllProducts] = useState([]);
   let products = [
     { id: 1, name: "prod1", description: "jhadsbfusjkan fksdufj,nsajfkjdsa" },
     { id: 2, name: "prod2", description: "jhadsbfusjkan fksdufj,nsajfkjdsa" },
   ];
+  const getProducts = async () => {
+    let data = await axios.get(`${URL}/getLatestProducts`);
+    console.log(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <Navv />
