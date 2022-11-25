8c673d5a3e5f0aec847244ae9c262922bd79e48c
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-11-25 14:50:00 -0500

    Pagination added to products

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Footer from './Footer'
 import Navv from './Navv'
 import ProductCard from './ProductCard'
+import Pagination from './Pagination'
 import { URL } from '../global'
 import axios from 'axios'
 import { CardGroup, Row } from 'reactstrap'
@@ -13,21 +14,23 @@ import { toast } from 'react-toastify'
 
 const Products = () => {
     const [apiData, setApiData] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    let PageSize = 10; //default page size = 10
+    const [totalCount, setTotalCount] = useState(0);
 
-    const getProducts = async () => {
+    const getProducts = async (page) => {
         try {
-            let data = await axios.get(`${URL}/getLatestProducts`)
-            console.log(data.data)
+            let data = await axios.get(`${URL}/getLatestProducts?pageSize=${PageSize}&pageNum=${page}`)
             setApiData(data.data)
+            setTotalCount(data.data.total)
         } catch (error) {
             toast.error('Something went wrong')
             console.log(error)
         }
     }
     useEffect(() => {
-        getProducts()
-    }, [])
-
+        getProducts(currentPage)
+    }, [currentPage])
     return (
         <>
             <Navv />
@@ -48,6 +51,13 @@ const Products = () => {
                     <div>No products found</div>
                 )}
             </Row>
+            <Pagination
+                className="pagination-bar"
+                currentPage={currentPage}
+                totalCount={totalCount}
+                pageSize={PageSize}
+                onPageChange={page => { setCurrentPage(page); getProducts(page); } }
+            />
             <Footer />
         </>
     )
