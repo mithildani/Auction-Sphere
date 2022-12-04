64ceccf4f6e95e38d17406e29d14438ace804217
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-12-04 18:15:40 -0500

    Minor Fix

--- backend/userms/__init__.py
+++ backend/userms/__init__.py
@@ -4,11 +4,9 @@ from userms.main.api import api
 from userms.main.database import db, migration
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
-app.config.from_object(main.settings[os.environ.get('APPLICATION_ENV', 'default')])
-
 from userms import models
+
+CORS(app, support_credentials=True)
 
 db.init_app(app)
 migration.init_app(app, db)
